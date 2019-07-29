c62f394ebfb8a8dad7afdfb445ef627749132966
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2019-07-29 19:35:46 +0200

    aospa: prebuilt: Welcome Abstruct!
    Signed-off-by: Hernán Castañón Álvarez <hernanderes34@gmail.com>
    Change-Id: I9785436475981b141369d4761b1fb1d0867f505e

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Abstruct
+PRODUCT_PACKAGES += \
+    Abstruct
+
 # Camera
 PRODUCT_PACKAGES += \
     GoogleCameraGo
