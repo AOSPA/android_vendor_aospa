7cbbdaec94287fd41cc9bf422c0524bd90fdc6af
Vinayak Soni <soniv@codeaurora.org>
2020-09-18 15:24:07 +0530

    aospa: soong: Create custom soong namespace for AOSP vs QVA
    This allows for conditionally differentiating between
    AOSP and QVA configuration.
    
    Change-Id: Ic2f04d6ed71a80948af7ec71a31ac673c678c052

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -2,6 +2,8 @@
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
+# PA Variables
+
 SOONG_CONFIG_NAMESPACES += aospaVarsPlugin
 
 SOONG_CONFIG_aospaVarsPlugin :=
@@ -61,3 +63,8 @@ GESTURE_SOONG_VARS := \
     TARGET_SINGLE_TAP_TO_WAKE_NODE
 
 $(foreach v,$(GESTURE_SOONG_VARS),$(eval $(call add-gesturevar-if-exist,$(v))))
+
+# Qualcomm variables
+SOONG_CONFIG_NAMESPACES += aosp_vs_qva
+SOONG_CONFIG_aosp_vs_qva += aosp_or_qva
+SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := qva
