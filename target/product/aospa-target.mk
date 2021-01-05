e683ef46b0202a97417ea407f5bbdeeceb28789c
Christian Oder <myself5@carbonrom.org>
2021-01-05 12:31:37 -0300

    aospa: taget: config: Add basic hotword permissions
     * Required for older hotword blobs
    
    Change-Id: I3a978ddda791a00274064e47348953c46006ace2

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -50,4 +50,5 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 # Permissions
 PRODUCT_COPY_FILES += \
     vendor/aospa/target/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    vendor/aospa/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/aospa/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
