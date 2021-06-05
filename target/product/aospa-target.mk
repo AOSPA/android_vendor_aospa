09a790f178742bcf79cc8c77e569f40ed045a950
Sam Mortimer <sam@mortimer.me.uk>
2021-06-04 23:25:41 -0700

    properties: Disable StrictMode in non-eng builds
    Change-Id: Ib052fba60e12497f79913c8030f59af40b6bc32f
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -185,6 +185,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/aospa/target/config/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
+# StrictMode
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Disable extra StrictMode features on all non-engineering builds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.strictmode.disable=true
+endif
+
 # SEPolicy
 $(call inherit-product, vendor/aospa/sepolicy/sepolicy.mk)
 
