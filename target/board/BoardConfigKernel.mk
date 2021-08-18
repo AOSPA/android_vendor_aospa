bc25f275b1309050bb10eca6c401cbd37ac93459
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:27:57 +0530

    aospa: Bring in kernel definition for ARM64.
    Change-Id: Idc20cb0ade1383c0522201f660efc4c5e7b976e9

--- target/board/BoardConfigKernel.mk
+++ target/board/BoardConfigKernel.mk
@@ -12,8 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Include our Qualcomm Board configuration.
-include device/qcom/common/BoardConfigQcom.mk
+# Architecture.
+TARGET_KERNEL_ARCH := arm64
 
-# Kernel configuration.
-include vendor/aospa/target/board/BoardConfigKernel.mk
+# Cross Compiler.
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
+# Enable LLVM Support.
+KERNEL_LLVM_SUPPORT := true
+
+# Qualcomm kernel.
+TARGET_COMPILE_WITH_MSM_KERNEL := true
