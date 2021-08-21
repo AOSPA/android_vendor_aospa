56567b1a801c9c8b171714d93b0e916cc17432c3
LuK1337 <priv.luk@gmail.com>
2021-08-21 20:19:46 +0530

    aospa: sepolicy: Add rules for FOD.
    Change-Id: Id9b8d33075b6f394710dfeeca88d73949bb837af

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -39,6 +39,9 @@ $(call inherit-product, vendor/aospa/target/product/packages.mk)
 # Include Properties makefile.
 $(call inherit-product, vendor/aospa/target/product/properties.mk)
 
+# Include SEPolicy makefile.
+$(call inherit-product, vendor/aospa/sepolicy/sepolicy.mk)
+
 # Include GMS, Modules, and Pixel features.
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_r.mk)
