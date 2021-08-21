330d486ac2a8fa37a6212b02e0e38c249bf97988
Vishalcj17 <vishalcj17@gmail.com>
2021-08-21 22:51:58 +0530

    aospa: Simplify AOSPA version and make it proper.
    Change-Id: I7b623c05799fa46de95e7011077f62b281016623

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# AOSPA Versioning.
+$(call inherit-product, vendor/aospa/target/product/version.mk)
+
 # Don't dexpreopt prebuilts. (For GMS).
 DONT_DEXPREOPT_PREBUILTS := true
 
