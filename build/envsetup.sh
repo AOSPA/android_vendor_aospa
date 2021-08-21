b4e3d64cd79e956a1065d49c6b290ccca8bb8d59
Anay Wadhera <awadhera@berkeley.edu>
2021-08-21 20:40:37 +0530

    aospa: build: Add a script to merge CAF.
    Change-Id: I1d36d27be1bf476c951e04de75c9ea85b80af9ab

---
+++ build/envsetup.sh
@@ -0,0 +1,14 @@
+function __print_aospa_functions_help() {
+cat <<EOF
+Additional Paranoid Android functions:
+- cafmerge:        Utility to merge CAF tags.
+EOF
+}
+
+function cafmerge()
+{
+    target_branch=$1
+    set_stuff_for_environment
+    T=$(gettop)
+    python3 $T/vendor/pa/scripts/merge-caf.py $target_branch
+}
