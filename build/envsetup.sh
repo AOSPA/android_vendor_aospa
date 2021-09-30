b0b80feaa9018668a7e192939c53d213e89e22be
Vishalcj17 <vishalcj@aospa.co>
2021-09-30 23:40:08 +0900

    aospa: build: tools: Bring-in Repopick script from AOSPA.
    Change-Id: I8eed4d9c039e4ced3b8bef2224140033ae330a80

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -2,6 +2,7 @@ function __print_aospa_functions_help() {
 cat <<EOF
 Additional Paranoid Android functions:
 - cafmerge:        Utility to merge CAF tags.
+- repopick:        Utility to fetch changes from Gerrit.
 EOF
 }
 
@@ -11,6 +12,12 @@ function cafmerge()
     set_stuff_for_environment
     T=$(gettop)
     python3 $T/vendor/aospa/scripts/merge-caf.py $target_branch
+}
+
+function repopick()
+{
+    T=$(gettop)
+    $T/vendor/aospa/build/tools/repopick.py $@
 }
 
 export SKIP_ABI_CHECKS="true"
