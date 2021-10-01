0beb8f896b5e9594624519a78625b4219e7cc399
Vishalcj17 <vishalcj@aospa.co>
2021-10-01 18:26:54 +0900

    target: board: Setup prebuilt_dtbo dependencies.
    Change-Id: I381c9f6e56ef5e61eb0aa8d733ed22168ab63a24

--- target/board/BoardConfigKernel.mk
+++ target/board/BoardConfigKernel.mk
@@ -24,3 +24,11 @@ KERNEL_LLVM_SUPPORT := true
 
 # Qualcomm kernel.
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Defining BOARD_PREBUILT_DTBOIMAGE here as AndroidBoardCommon.mk
+# is included before build/core/Makefile, where it is required to
+# set the dependencies on prebuilt_dtbo.img based on definition of
+# BOARD_PREBUILT_DTBOIMAGE
+ifneq ($(strip $(BOARD_KERNEL_SEPARATED_DTBO)),)
+BOARD_PREBUILT_DTBOIMAGE := $(PRODUCT_OUT)/prebuilt_dtbo.img
+endif
