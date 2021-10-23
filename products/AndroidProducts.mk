AOSPA/android_vendor_pa

Welcome Miatoll targets
what does miatoll mean?
Ans: Xiaomi released a good number of devices with SDM720G SoC, they also have same display, dimensions, battery capacity, similar cameras, etc.

Redmi Note 9S / Redmi Note 9 Pro IN (curtana)
Redmi Note 9 Pro Global (joyeuse)
Redmi Note 9 Pro Max (excalibur)
POCO M2 Pro (gram)

Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
Change-Id: I348122e6c588b152b911909ba7b07ea9e0d6a39e

products/AndroidProducts.mk
@@ -26,6 +26,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/jasmine_sprout/pa_jasmine_sprout.mk \
     $(LOCAL_DIR)/laurel_sprout/pa_laurel_sprout.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
+    $(LOCAL_DIR)/miatoll/pa_miatoll.mk \
     $(LOCAL_DIR)/mini5g/pa_mini5g.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
     $(LOCAL_DIR)/oneplus6/pa_oneplus6.mk \
@@ -55,6 +56,7 @@ COMMON_LUNCH_CHOICES += \
     pa_jasmine_sprout-userdebug \
     pa_laurel_sprout-userdebug \
     pa_mata-userdebug \
+    pa_miatoll-userdebug \
     pa_mini5g-userdebug \
     pa_oneplus6-userdebug \
     pa_oneplus7-userdebug \

AOSPA/android_vendor_aospa

products/AndroidProducts.mk
@@ -14,10 +14,12 @@
 
 # Product Makefiles
 PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/miatoll/aospa_miatoll.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
+    aospa_miatoll-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug