AOSPA/android_vendor_pa

pa: Add Nubia Mini 5G
Change-Id: Iea62c9cf8b703d7e7d8439c9174b573824b6bc5b

products/AndroidProducts.mk
@@ -35,6 +35,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lavender/pa_lavender.mk \
     $(LOCAL_DIR)/mata/pa_mata.mk \
     $(LOCAL_DIR)/mido/pa_mido.mk \
+    $(LOCAL_DIR)/mini5g/pa_mini5g.mk \
     $(LOCAL_DIR)/onc/pa_onc.mk \
     $(LOCAL_DIR)/oneplus3/pa_oneplus3.mk \
     $(LOCAL_DIR)/oneplus5/pa_oneplus5.mk \
@@ -76,6 +77,7 @@ COMMON_LUNCH_CHOICES += \
     pa_lavender-userdebug \
     pa_mata-userdebug \
     pa_mido-userdebug \
+    pa_mini5g-userdebug \
     pa_onc-userdebug \
     pa_oneplus3-userdebug \
     pa_oneplus6-userdebug \

AOSPA/android_vendor_aospa

products/AndroidProducts.mk
@@ -14,10 +14,12 @@
 
 # Product Makefiles
 PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
+    aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug