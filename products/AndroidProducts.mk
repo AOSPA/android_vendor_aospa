c2549fba756b3c2d494bdf33ad55543fcc2333d4
Chris Crump <chris@cypheros.co>
2021-11-02 21:39:18 -0400

    aospa: products: Introduce Raviole (Pixel 6/6 Pro)
    Change-Id: Id4f418ef6c3b26aa6bb2ae169ffd74415ddb0a77

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -14,10 +14,12 @@
 
 # Product Makefiles
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+	$(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
+	$(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+	$(LOCAL_DIR)/raviole/aospa_raviole.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
-    aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+	aospa_oneplus9-userdebug \
+	aospa_oneplus9pro-userdebug \
+	aospa_raviole-userdebug
