b9c3cba1e629767d3ccb0c1820e32fcfc3a104cf
Chris Crump <chris@cypheros.co>
2021-11-07 16:12:49 -0500

    aospa: products: Introduce Pixel 6/6 Pro (Oriole/Raven)
    Change-Id: Id4f418ef6c3b26aa6bb2ae169ffd74415ddb0a77

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -14,10 +14,14 @@
 
 # Product Makefiles
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+	$(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
+	$(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+	$(LOCAL_DIR)/oriole/aospa_oriole.mk \
+	$(LOCAL_DIR)/raven/aospa_raven.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
-    aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+	aospa_oneplus9-userdebug \
+	aospa_oneplus9pro-userdebug \
+	aospa_oriole-userdebug \
+	aospa_raven-userdebug
