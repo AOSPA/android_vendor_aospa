945cc3422208edcf972a5254e042e551c28b9ba5
Andrey Sprynga <rtx4d@aospa.co>
2021-11-28 01:59:46 +0700

    aospa: products: Add Xiaomi MI845 targets
    Xiaomi Pocophone F1 (beryllium)
    Xiaomi Mi 8 (dipper)
    Xiaomi Mi 8 Pro ( equuleus)
    Xiaomi Mi 8 Explorer Edition (ursa)
    Xiaomi Mi MIX 2s (polaris)
    Xioami Mi MIX 3 (perseus)
    
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>
    Change-Id: Ie044f2decea7b40c043a737a535761098dbbbbaf

---
+++ products/beryllium/aospa_beryllium.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2020-2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (aospa_beryllium,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Override build properties
+PRODUCT_NAME := aospa_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Poco F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+endif
