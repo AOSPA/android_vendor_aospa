023c7ae5f5efee2a05495d07ea9a4cbeeee3067b
Vishalcj17 <vishalcj@aospa.co>
2021-12-11 05:38:56 +0900

    packages: Build VNDFWK system and vendor variants.
    Change-Id: I9f4e5781f007be0367f1ff5d6d92adbc2b171e83

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -40,5 +40,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libqti_vndfwk_detect \
+    libqti_vndfwk_detect_system \
+    libqti_vndfwk_detect_vendor \
+    libvndfwk_detect_jni.qti_system \
+    libvndfwk_detect_jni.qti_vendor \
     libvndfwk_detect_jni.qti.vendor \
     libqti_vndfwk_detect.vendor
