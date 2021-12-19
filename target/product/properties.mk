03a1637f68efcdc2218f7edf23d2fa6d69648e94
Vishalcj17 <vishalcj@aospa.co>
2021-12-20 03:41:57 +0900

    properties: Goodbye blurs!
     * Unused, we had this in android 11 where the notification panel and
       recents were transparent. Now its dependent on monet (themeing engine).
    
     * Moreover, we would save some GPU resources.
    
    Change-Id: I0e161c89734079e710531f3cacb3e2cc737c444a

--- target/product/properties.mk
+++ target/product/properties.mk
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Blurs
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
-
 # Enable Navigation gestures.
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
