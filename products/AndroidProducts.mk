c01819acf4aebbbd5fd74824c25f7195b138ca4b
Gagan Malvi <malvigagan@gmail.com>
2021-12-23 05:42:50 +0100

    pa: products: Welcome, Redmi 9T
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>
    Change-Id: I4382918308145f47d3e6ae7931a90f27b6a9d9cd

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,9 +15,11 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+    $(LOCAL_DIR)/lime/aospa_lime.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+    aospa_oneplus9pro-userdebug \
+    aospa_lime-userdebug
