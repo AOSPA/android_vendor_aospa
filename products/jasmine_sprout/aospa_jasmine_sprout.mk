bc4a0df8e5417dcd5a6b9737f98e2e6c2464cf4c
OdSazib <odsazib@gmail.com>
2021-12-25 10:50:09 +0600

    aospa: products: Add Xiaomi Mi A2
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: Ie7efe9c0845e9544bf4a91ad59c1781a2d643fce

---
+++ products/jasmine_sprout/aospa_jasmine_sprout.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_jasmine_sprout,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := jasmine_sprout
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Mi A2
+PRODUCT_NAME := aospa_jasmine_sprout
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jasmine-user 8.1.0 OPM1.171019.011 V9.6.16.0.ODIMIFE release-keys"
+
+endif
