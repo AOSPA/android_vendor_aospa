7cc81120b44bda4983a6907f81dc8fa1a2364ff2
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-27 15:19:45 +0530

    aospa: products: Add support for Xiaomi Mix 4.
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: I1cf867a33d6b64e50cce65a689e4a0aeac3230d4

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,6 +15,7 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
+    $(LOCAL_DIR)/odin/aospa_odin.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
     $(LOCAL_DIR)/X00TD/aospa_X00TD.mk \
@@ -23,6 +24,7 @@ PRODUCT_MAKEFILES += \
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_mini5g-userdebug \
+    aospa_odin-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug \
     aospa_X00TD-userdebug \
