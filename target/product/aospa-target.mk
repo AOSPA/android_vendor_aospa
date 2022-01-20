c0aaef5e6a60768e2591dcab2f6f8616660ee811
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-20 18:56:28 +0000

    vendor: Import Google Game Dashboard [3/3]
    [1] - From SD1A.210817.037
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ic96ad15c4c9f46fb0aee7bdf56e76d91bd877355

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -61,6 +61,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    vendor/aospa/target/config/permissions/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml \
     vendor/aospa/target/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     vendor/aospa/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/aospa/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
