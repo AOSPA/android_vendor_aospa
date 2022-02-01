ab6dfa0a94300bc182cfe845aa6d14206bde0cb5
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-02-01 20:23:30 +0600

    aospa: products: Add support for Xiaomi Miatoll.
    What does miatoll mean?
    Ans: Xiaomi released a good number of devices with SDM720G SoC, they also have same display, dimensions, battery capacity, similar cameras, etc.
    
    POCO M2 Pro (gram)
    Redmi Note 9S / Redmi Note 9 Pro IN (curtana)
    Redmi Note 9 Pro Global (joyeuse)
    Redmi Note 9 Pro Max (excalibur)
    
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: I7a5698542e9edf97387552abddcc564e30e28738

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,6 +18,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/apollo/aospa_apollo.mk \
     $(LOCAL_DIR)/bluejay/aospa_bluejay.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
+    $(LOCAL_DIR)/miatoll/aospa_miatoll.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
@@ -30,6 +31,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_apollo-userdebug \
     aospa_bluejay-userdebug \
     aospa_lmi-userdebug \
+    aospa_miatoll-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug \
