209d45e4c77f2d9f15947c58ccf17621da64b7f2
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-02-01 20:23:30 +0600

    aospa: products: Add support for Xiaomi Miatoll.
    What does miatoll mean?
    Ans: Xiaomi released a good number of devices with SDM720G SoC, they also have same display, dimensions, battery capacity, similar cameras, etc.
    
    POCO M2 Pro (gram)
    Redmi Note 9S / Redmi Note 9 Pro IN (curtana)
    Redmi Note 9 Pro Global (joyeuse)
    Redmi Note 9 Pro Max (excalibur)
    
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: I7a5698542e9edf97387552abddcc564e30e28738

---
+++ products/miatoll/aospa_miatoll.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Check for target product
+ifeq (aospa_miatoll,$(TARGET_PRODUCT))
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/xiaomi/miatoll/miatoll.mk)
+
+ # Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Device identifier
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := miatoll
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Miatoll
+PRODUCT_NAME := aospa_miatoll
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
