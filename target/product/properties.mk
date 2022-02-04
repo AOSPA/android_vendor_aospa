47db41107a4d6ab78df98e90f65c26e88181eb4d
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-02-04 10:58:06 +0530

    properties: optimize package manager dex flags
    * first-boot and ab-ota are set to verify to reduce first
      boot times as well as time spent when updating in system
    * we are installing with speed-profile to have relatively
      fast install times without losing too much runtime perf
    * once device is in idle+charging, bg dexopt will compile
      for maximum runtime performance
    
    Change-Id: Ib7d9ea89fdbaedf8a3bd7212ca97df09e8f1dac4
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- target/product/properties.mk
+++ target/product/properties.mk
@@ -32,6 +32,18 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true
 
+# Optimise package manager dex flags
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    pm.dexopt.boot=verify \
+    pm.dexopt.first-boot=verify \
+    pm.dexopt.install=speed-profile \
+    pm.dexopt.bg-dexopt=everything
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    pm.dexopt.ab-ota=verify
+endif
+
 # Enable one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
