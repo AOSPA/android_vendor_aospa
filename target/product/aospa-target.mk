a42d0629d86a324482efae3e8998cd371ecb560b
Omkar Chandorkar <forumomkar@gmail.com>
2022-02-27 09:04:17 +0000

    Revert "target: Enable minimize java debug info"
    This reverts commit 8e713d86d2a9804e0a9a1d14e69472b7413a9923.
    
    Reason for revert: qssi12 WfdCommon refuses to get dex2oat'd after this
    
    /usr/bin/addr2line: DWARF error: section .debug_info is larger than its filesize! (0x545abe vs 0x3d0450)
    
    Change-Id: Id290d82ff5a55f321945aa6a1f0a598e25621b17

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -67,8 +67,3 @@ PRODUCT_COPY_FILES += \
 
 # Skip boot JAR checks.
 SKIP_BOOT_JARS_CHECK := true
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
