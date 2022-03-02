700e0636d79f761c19e9f7df070e41f2eb52ffb9
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-02 19:16:55 +0900

    packages: remove non-existent build targets
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I1eaf40a0583a647aebd425086a95d0f09f4b6f50

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -32,8 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Paranoid Packages
 PRODUCT_PACKAGES += \
-    ParanoidPapers \
-    ParanoidQuickStep
+    ParanoidPapers
 
 # Theme Picker
 PRODUCT_PACKAGES += \
@@ -52,8 +51,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect_system \
-    libqti_vndfwk_detect_vendor \
     libvndfwk_detect_jni.qti_system \
-    libvndfwk_detect_jni.qti_vendor \
     libvndfwk_detect_jni.qti.vendor \
     libqti_vndfwk_detect.vendor
