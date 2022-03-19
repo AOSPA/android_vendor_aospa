d4994ef76828db7fea568ca1297dae92ba25db7f
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-03-19 14:37:16 +0600

    aospa: bootanimation: Simplify copying logic
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: I66c99aaab083c31ea6c782b166745556aaa4932d

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -5,19 +5,8 @@
 #
 
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/aospa/bootanimation/720/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/aospa/bootanimation/1080/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/aospa/bootanimation/1440/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
-     PRODUCT_COPY_FILES += vendor/aospa/bootanimation/2160/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+ifneq ($(filter $(TARGET_BOOT_ANIMATION_RES),720 1080 1440 2160),)
+    PRODUCT_COPY_FILES += vendor/aospa/bootanimation/$(TARGET_BOOT_ANIMATION_RES)/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
-    else
-        $(warning "Current bootanimation res is not supported, forcing 1080p")
-    endif
-    PRODUCT_COPY_FILES += vendor/aospa/bootanimation/1080/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    $(error "Invalid bootanimation resolution: $(TARGET_BOOT_ANIMATION_RES). Define TARGET_BOOT_ANIMATION_RES to 720/1080/1440/2160.")
 endif
