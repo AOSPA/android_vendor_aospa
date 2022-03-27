68f2e0eeb9f2995b367e911cf59cb93ba4733a11
Roman Rihter <teledurak@gmail.com>
2022-03-27 23:45:21 +0300

    products: add Meizu 16th family
    Change-Id: I6b10b87900e9ef61a39a43e05c437232b8f07da2

---
+++ products/m1882/aospa_m1882.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Device supports 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Use full-featured configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Launched with API 27
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# m1882 configuration
+$(call inherit-product, device/meizu/m1882/m1882.mk)
+
+# AOSPA configuration
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := aospa_m1882
+PRODUCT_BRAND := Meizu
+PRODUCT_DEVICE := m1882
+PRODUCT_MANUFACTURER := Meizu
+PRODUCT_MODEL := 16th
+
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="16th" \
+    PRODUCT_NAME="meizu_16th_CN" \
+    PRIVATE_BUILD_DESC="meizu_16th_CN-user 10 QKQ1.191222.002 1594833800 release-keys"
+
+BUILD_FINGERPRINT := meizu/qssi/qssi:10/QKQ1.191222.002/1594833800:user/release-keys 
