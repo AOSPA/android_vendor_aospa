39b232a66aa822c36ad531a36d276f076813a56c
Roman Rihter <teledurak@gmail.com>
2022-05-11 21:31:14 +0300

    lineage_kernel: Add SDLLVM support
    Can be disabled with TARGET_KERNEL_SDCLANG_COMPILE flag
    
    Change-Id: I491cce64ebd9e99bec5218f77f2840b47e9b7618

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -30,6 +30,8 @@
 #
 #   TARGET_KERNEL_CLANG_COMPILE        = Compile kernel with clang, defaults to true
 #
+#   TARGET_KERNEL_SDCLANG_COMPILE      = Compile kernel with sdclang, defaults to true
+#
 #   TARGET_KERNEL_CLANG_VERSION        = Clang prebuilts version, optional, defaults to clang-stable
 #
 #   TARGET_KERNEL_CLANG_PATH           = Clang prebuilts path, optional
@@ -223,20 +225,36 @@ ifeq ($(or $(FULL_RECOVERY_KERNEL_BUILD), $(FULL_KERNEL_BUILD)),true)
 # Add host bin out dir to path
 PATH_OVERRIDE := PATH=$(KERNEL_BUILD_OUT_PREFIX)$(HOST_OUT_EXECUTABLES):$$PATH
 ifneq ($(TARGET_KERNEL_CLANG_COMPILE),false)
-    ifneq ($(TARGET_KERNEL_CLANG_VERSION),)
-        KERNEL_CLANG_VERSION := clang-$(TARGET_KERNEL_CLANG_VERSION)
+	ifneq ($(TARGET_KERNEL_SDCLANG_COMPILE), false)
+		# Use previous directory as sdclang path because
+		# SDCLANG_PATH defines path to bin directory under sdclang.
+        ifeq ($(shell echo $(SDCLANG_PATH) | head -c 1),/)
+            TARGET_KERNEL_CLANG_PATH ?= $(SDCLANG_PATH)/..
+        else
+            TARGET_KERNEL_CLANG_PATH ?= $(shell pwd)/$(SDCLANG_PATH)/..
+        endif
+
+        ifeq ($(KERNEL_ARCH),arm64)
+            KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
+        else ifeq ($(KERNEL_ARCH),arm)
+            KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=arm-linux-gnueabihf
+        endif	
     else
-        # Use the default version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
-        KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
-    endif
-    TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(KERNEL_CLANG_VERSION)
-    ifeq ($(KERNEL_ARCH),arm64)
-        KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
-    else ifeq ($(KERNEL_ARCH),arm)
-        KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=arm-linux-gnu-
-    else ifeq ($(KERNEL_ARCH),x86)
-        KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=x86_64-linux-gnu-
-    endif
+		ifneq ($(TARGET_KERNEL_CLANG_VERSION),)
+			KERNEL_CLANG_VERSION := clang-$(TARGET_KERNEL_CLANG_VERSION)
+		else
+			# Use the default version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
+			KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
+		endif
+		TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(KERNEL_CLANG_VERSION)
+		ifeq ($(KERNEL_ARCH),arm64)
+			KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
+		else ifeq ($(KERNEL_ARCH),arm)
+			KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=arm-linux-gnu-
+		else ifeq ($(KERNEL_ARCH),x86)
+			KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=x86_64-linux-gnu-
+		endif
+	endif
     PATH_OVERRIDE += PATH=$(TARGET_KERNEL_CLANG_PATH)/bin:$$PATH LD_LIBRARY_PATH=$(TARGET_KERNEL_CLANG_PATH)/lib64:$$LD_LIBRARY_PATH
     ifeq ($(KERNEL_CC),)
         KERNEL_CC := CC="$(CCACHE_BIN) clang"
