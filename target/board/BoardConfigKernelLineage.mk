1fe00da32499bfcab1b4b1b6d95f958702914781
Roman Rihter <teledurak@gmail.com>
2022-05-11 21:43:39 +0300

    lineage_kernel: Remove tools-lineage usage
    Change-Id: I329b766e212226254efd02e00c0abdafed5c13d6

--- target/board/BoardConfigKernelLineage.mk
+++ target/board/BoardConfigKernelLineage.mk
@@ -124,15 +124,9 @@ ifneq ($(TARGET_KERNEL_ADDITIONAL_FLAGS),)
   KERNEL_MAKE_FLAGS += $(TARGET_KERNEL_ADDITIONAL_FLAGS)
 endif
 
-TOOLS_PATH_OVERRIDE := \
-    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/lib:$$LD_LIBRARY_PATH \
-    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-lineage/common/perl-base
-
 ifeq ($(KERNEL_ARCH),arm64)
   # Add 32-bit GCC to PATH so that arm-linux-androidkernel-as is available for CONFIG_COMPAT_VDSO
-  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$(KERNEL_TOOLCHAIN_arm):$$PATH
-else
-  TOOLS_PATH_OVERRIDE += PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$$PATH
+  TOOLS_PATH_OVERRIDE += PATH=$(KERNEL_TOOLCHAIN_arm):$$PATH
 endif
 
 # Set DTBO image locations so the build system knows to build them
