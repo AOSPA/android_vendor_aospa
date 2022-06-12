72db679fbf9790eaf286e0fa873e87ca4bb943d2
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-06-12 07:34:41 +0530

    overlay: Import OnePlus Sans font
    Change-Id: Ib62b19c1506197f06cc1f99b4cde475e0c380cab
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     FontHarmonySansOverlay \
     FontInterOverlay \
     FontManropeOverlay \
+    FontOnePlusOverlay  \
     FontRobotoOverlay \
     FontUrbanistOverlay \
     IconPackCircularAndroidOverlay \
