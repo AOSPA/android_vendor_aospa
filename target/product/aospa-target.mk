7b4491093678cc0094034dd104fb924ed9c76634
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-06-17 12:35:22 +0900

    aospa: target: flip the check for flattened apex
    * this will set updatable_apex to true by default
    * those who want to disable it can still set the flag
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I11f1a8a4229aa4a93b80c11f46fe6af7a152704b

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -50,7 +50,7 @@ $(call inherit-product, vendor/aospa/sepolicy/sepolicy.mk)
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
 
-ifeq ($(TARGET_FLATTEN_APEX), false)
+ifneq ($(TARGET_FLATTEN_APEX), true)
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_s.mk)
 else
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_s_flatten_apex.mk)
