70076bf9e64393d526f4823f2f5c28aa477f5ed4
Tushar Mahajan <mahajant99@gmail.com>
2022-06-23 23:55:43 +0530

    aospa: products: Introduce Xiaomi 10T / 10T Pro
    Change-Id: I0e97cdece941e0b65e4f56a6c7a0c8460e4470ee
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,6 +15,7 @@
 # Product Makefiles
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/alioth/aospa_alioth.mk \
+    $(LOCAL_DIR)/apollo/aospa_apollo.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
@@ -22,6 +23,7 @@ PRODUCT_MAKEFILES += \
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
+    aospa_apollo-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug
