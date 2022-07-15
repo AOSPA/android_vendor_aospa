8a90326dcaccab9a0f18650c6d522b950c08d130
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2022-07-15 20:15:00 +0200

    products: Introduce Nothing
    * Nothing Phone 1
    
    Change-Id: I9adfdd3880e10ab1dcc30b6d5ca527b4554b96db

---
+++ products/phone1/aospa_phone1.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_phone1,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/nothing/phone1/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := Nothing
+PRODUCT_DEVICE := phone1
+PRODUCT_MANUFACTURER := Nothing
+PRODUCT_MODEL := Nothing phone (1)
+PRODUCT_NAME := aospa_phone1
+
+# Bootanimation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Override device name for Play Store.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=Phone1
+
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
+
+endif
