1df0fd50e49776b3a23c15932d3e88954b28d37e
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-15 03:59:18 +0000

    [WIP] aospa: Move Qualcomm specific configs to qcom/common
    [1] - Now we have Pixel 6 here, which doesn't use any Qualcommon,
          so let's move it to the appropriate place.
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I534ce11753b5cb7a7b892c5e4c441951b312369b

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -21,9 +21,6 @@ $(call inherit-product, vendor/aospa/bootanimation/bootanimation.mk)
 # Don't dexpreopt prebuilts. (For GMS).
 DONT_DEXPREOPT_PREBUILTS := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN += vendor/aospa/target/config/config.fs
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
      vendor/aospa/target/config/aospa_vendor_framework_compatibility_matrix.xml
@@ -61,9 +58,7 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/aospa/target/config/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     vendor/aospa/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
-    vendor/aospa/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     vendor/aospa/target/config/permissions/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
 
 # Sensitive phone numbers and APN configurations
