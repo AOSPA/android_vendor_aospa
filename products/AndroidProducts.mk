7aa5b43b754964b967a214e35fd31e222b0ac341
Mesquita <mesquita@aospa.co>
2022-07-24 00:32:17 +0530

    aospa: products: Introduce Xiaomi 12x
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: Ie3e699d4928393fb8fdcd9719bd4378d50eaebfa

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,7 +18,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+    $(LOCAL_DIR)/psyche/aospa_psyche.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -26,4 +27,5 @@ COMMON_LUNCH_CHOICES += \
     aospa_lmi-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+    aospa_oneplus9pro-userdebug \
+    aospa_psyche-userdebug
