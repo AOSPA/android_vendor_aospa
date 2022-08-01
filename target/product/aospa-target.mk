a8022c4acb42e0db775c98680d046344093d2578
Omkar Chandorkar <gotenksIN@aospa.co>
2022-08-01 17:46:24 +0900

    aospa: switch to partner_gms
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: Iaabf0325293224ab4be7f75345f8064af17b71e7

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -58,7 +58,16 @@ $(call inherit-product, vendor/aospa/target/product/properties.mk)
 $(call inherit-product, vendor/aospa/sepolicy/sepolicy.mk)
 
 # Include GMS, Modules, and Pixel features.
-$(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product, vendor/partner_gms/products/gms.mk)
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/GoogleExtShared/GoogleExtShared.apk \
+    system/app/GooglePrintRecommendationService/GooglePrintRecommendationService.apk \
+    system/etc/permissions/privapp-permissions-google-system.xml \
+    system/etc/sysconfig/google-hiddenapi-package-allowlist.xml \
+    system/priv-app/GooglePackageInstaller/GooglePackageInstaller.apk \
+    system/priv-app/TagGoogle/TagGoogle.apk
+
 $(call inherit-product, vendor/google/pixel/config.mk)
 
 ifneq ($(TARGET_FLATTEN_APEX), true)
