228b6142c6beead3807de93b42f2e0a22c1f61f7
enesykaya <enesykaya@gmail.com>
2022-08-12 20:43:43 +0300

    aospa: products: Introduce OnePlus 5/5T
    Change-Id: I9560a1ca878b45dfa4b7aefe529a16e50681beae
    Signed-off-by: enesykaya <enesykaya@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -24,6 +24,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
     $(LOCAL_DIR)/miatoll/aospa_miatoll.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
+    $(LOCAL_DIR)/oneplus5/aospa_oneplus5.mk \
+    $(LOCAL_DIR)/oneplus5t/aospa_oneplus5t.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
     $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
     $(LOCAL_DIR)/oneplus9r/aospa_oneplus9r.mk \
@@ -46,6 +48,8 @@ COMMON_LUNCH_CHOICES += \
     aospa_lmi-userdebug \
     aospa_miatoll-userdebug \
     aospa_mini5g-userdebug \
+    aospa_oneplus5-userdebug \
+    aospa_oneplus5t-userdebug \
     aospa_oneplus9-userdebug \
     aospa_oneplus9pro-userdebug \
     aospa_oneplus9r-userdebug \
