cef9984c359a80c007799f0c73645834178dee45
Chris Crump <chriscrawford893@gmail.com>
2022-09-05 15:13:41 -0400

    aospa: products: Introduce Pixel 6/6 Pro/6a (Oriole/Raven/Bluejay)
    Change-Id: Ie8e373b1b3384520874a3938b7ff64e17400d5b1

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,16 +16,22 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/alioth/aospa_alioth.mk \
     $(LOCAL_DIR)/apollo/aospa_apollo.mk \
+    $(LOCAL_DIR)/bluejay/aospa_bluejay.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk
+    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
+    $(LOCAL_DIR)/oriole/aospa_oriole.mk \
+    $(LOCAL_DIR)/raven/aospa_raven.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
     aospa_apollo-userdebug \
+    aospa_bluejay-userdebug \
     aospa_lmi-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug
+    aospa_oneplus9pro-userdebug \
+    aospa_oriole-userdebug \
+    aospa_raven-userdebug
