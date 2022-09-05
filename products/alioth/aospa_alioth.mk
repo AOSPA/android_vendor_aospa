b0fe21aafc48a7ceecaf84165d5c4817df783497
Omkar Chandorkar <forumomkar@gmail.com>
2022-09-05 09:04:07 +0000

    Revert "products: alioth: add property overrides to match POCO F3"
    This reverts commit 54003ec9f28f29dbb10f4d2d7463d825eca7099b.
    
    Reason for revert: this is no bueno because no single model works for all
    
    Change-Id: Icf57780e30379de178e28044bbbbefb77d5d6939

--- products/alioth/aospa_alioth.mk
+++ products/alioth/aospa_alioth.mk
@@ -30,11 +30,6 @@ PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11AG
 
-PRODUCT_SYSTEM_DEVICE := alioth
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=alioth
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Boot animation resolution.
