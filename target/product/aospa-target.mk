8ff8986fb573f2150071dea81312a6a0992cedbf
Omkar Chandorkar <gotenksIN@aospa.co>
2022-10-20 07:40:24 +0530

    target: make mainline module inherit platform agnostic
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Change-Id: I05ab4340987573a1f93ed01df19a8ddf60afaa7f

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -62,9 +62,9 @@ $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
 
 ifneq ($(TARGET_FLATTEN_APEX), true)
-$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_s.mk)
+$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules.mk)
 else
-$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_s_flatten_apex.mk)
+$(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_flatten_apex.mk)
 endif
 
 # Move Wi-Fi modules to vendor.
