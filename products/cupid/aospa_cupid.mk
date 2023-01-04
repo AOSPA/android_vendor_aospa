b7354315b4707bd9486cd275a8d622ff40715d48
Arian <arian.kulmer@web.de>
2023-01-04 14:22:45 +0100

    Add cupid
    Change-Id: I3418ce02d9e662bf3bc5a5ae998849486d6f431c

---
+++ products/cupid/aospa_cupid.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq (aospa_cupid,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/cupid/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := cupid
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2201123G
+PRODUCT_NAME := aospa_cupid
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=cupid \
+    TARGET_PRODUCT=cupid
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
