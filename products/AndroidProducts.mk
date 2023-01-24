ce09cf94a1424b21ed9b6fc7bb79004d9e641c6c
Jprimero15 <jprimero155@gmail.com>
2023-01-24 10:03:10 +0000

    products: Introduce Xioami SDM439 (mi439)
    Unified devices:
    * Redmi 8/8A/pro/dual and Redmi 7A
    
    Change-Id: I75706418f01ab0abb5593b6bb78acb4e914b67a3
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -22,6 +22,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bluejay/aospa_bluejay.mk \
     $(LOCAL_DIR)/cheetah/aospa_cheetah.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
+    $(LOCAL_DIR)/mi439/aospa_mi439.mk \
     $(LOCAL_DIR)/miatoll/aospa_miatoll.mk \
     $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
@@ -44,6 +45,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_bluejay-userdebug \
     aospa_cheetah-userdebug \
     aospa_lmi-userdebug \
+    aospa_mi439-userdebug \
     aospa_miatoll-userdebug \
     aospa_mini5g-userdebug \
     aospa_oneplus9-userdebug \
