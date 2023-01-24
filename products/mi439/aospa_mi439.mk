d6af167c28f551bd2f005ff2c24e2d05e2337b4c
Jprimero15 <jprimero155@gmail.com>
2023-01-24 10:03:10 +0000

    products: Introduce Xioami SDM439 (mi439)
    Unified devices:
    * Redmi 8/8A/pro/dual and Redmi 7A
    
    Change-Id: I75706418f01ab0abb5593b6bb78acb4e914b67a3
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ products/mi439/aospa_mi439.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_mi439, $(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from mi439 device configuration.
+$(call inherit-product, device/xiaomi/mi439/device.mk)
+
+# Inherit from common AOSPA configuration
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := aospa_mi439
+PRODUCT_DEVICE := mi439
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI SDM439
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 720
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+
+endif
