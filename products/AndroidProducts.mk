af501c3682e71f92cbce25483760f8651b575155
blurzyy <selster02@gmail.com>
2023-03-16 08:58:42 +0800

    aospa: products: Introduce Redmi Note 10 (sunny)
    Change-Id: Iffaddea01a24f279dcd6a01b567188d0eaea5629
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -47,6 +47,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
+    $(LOCAL_DIR)/sunny/aospa_sunny.mk \
     $(LOCAL_DIR)/vayu/aospa_vayu.mk
 
 # Lunch targets
@@ -84,4 +85,5 @@ COMMON_LUNCH_CHOICES += \
     aospa_phone1-userdebug \
     aospa_raven-userdebug \
     aospa_spes-userdebug \
+    aospa-sunny-userdebug \
     aospa_vayu-userdebug
