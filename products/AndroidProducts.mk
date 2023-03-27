00c9bdd815d1bdbc9e9512ea7da0f86b072c41dd
ShelbyHell <shelbyhell@proton.me>
2023-03-27 17:26:51 +0000

    products: Introduce Redmi 9T/POCO M3 (juice).
    Change-Id: Id31200d41d56645f87689baf4ee402cad577d172
    Signed-off-by: ShelbyHell <shelbyhell@proton.me>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -36,7 +36,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
-    $(LOCAL_DIR)/spes/aospa_spes.mk
+    $(LOCAL_DIR)/spes/aospa_spes.mk \
+    $(LOCAL_DIR)/juice/aospa_juice.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -62,4 +63,5 @@ COMMON_LUNCH_CHOICES += \
     aospa_phoenix-userdebug \
     aospa_phone1-userdebug \
     aospa_raven-userdebug \
-    aospa_spes-userdebug
+    aospa_spes-userdebug \
+    aospa_juice-userdebug
