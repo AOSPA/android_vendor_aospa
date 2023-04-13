342ef346450621cee1c5b4aca48c6d6e4b6257d5
Mashopy <eliasgheeraert@gmail.com>
2023-04-13 21:00:24 +0200

    aospa: board: Use Linaro GNU GCC toolchains as cross compilers prefix
    Change-Id: Ia4ca32b6418a3dab9fad771f5c85b8ac350fa5ca

--- target/board/BoardConfigKernel.mk
+++ target/board/BoardConfigKernel.mk
@@ -16,8 +16,8 @@
 TARGET_KERNEL_ARCH := arm64
 
 # Cross Compiler.
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 # Enable LLVM Support.
 KERNEL_LLVM_SUPPORT := true
