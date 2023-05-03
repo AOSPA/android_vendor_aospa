7477a8476b7ffa0fba6328970d961afa19dca43c
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-05-03 15:21:57 +0000

    products: Add Xiaomi POCO X3/X3 NFC
    Change-Id: Ife871c2ffd7847424336e956f658033b42ee650a

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -45,7 +45,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
-    $(LOCAL_DIR)/spes/aospa_spes.mk
+    $(LOCAL_DIR)/spes/aospa_spes.mk \
+    $(LOCAL_DIR)/surya/aospa_surya.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -80,4 +81,5 @@ COMMON_LUNCH_CHOICES += \
     aospa_phoenix-userdebug \
     aospa_phone1-userdebug \
     aospa_raven-userdebug \
-    aospa_spes-userdebug
+    aospa_spes-userdebug \
+    aospa_surya-userdebug
