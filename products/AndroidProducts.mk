8550c7f9df009779a19eb79f236d2cc4852afaad
Juhyung Park <qkrwngud825@gmail.com>
2023-05-18 16:29:58 +0900

    products: oneplus9: switch to unified OOS 11
    Change-Id: Ia792f8a701f9ba4b15333a7f9bc7ea8f96900f6c
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -39,7 +39,6 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus7t/aospa_oneplus7t.mk \
     $(LOCAL_DIR)/oneplus7tpro/aospa_oneplus7tpro.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9pro/aospa_oneplus9pro.mk \
     $(LOCAL_DIR)/oneplus9r/aospa_oneplus9r.mk \
     $(LOCAL_DIR)/oriole/aospa_oriole.mk \
     $(LOCAL_DIR)/panther/aospa_panther.mk \
@@ -77,7 +76,6 @@ COMMON_LUNCH_CHOICES += \
     aospa_oneplus7t-userdebug \
     aospa_oneplus7tpro-userdebug \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9pro-userdebug \
     aospa_oneplus9r-userdebug \
     aospa_oriole-userdebug \
     aospa_panther-userdebug \
