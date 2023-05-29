4f3aa2eb9050ae6bb59e48c087986f61542aa533
Daniel Rosenberg <drosen@google.com>
2023-05-29 10:19:44 +0530

    Products: jasmine_sprout: Use emulated storage
    Sdcardfs in deprecated
    
    Enable Project ID Quota support on userdata.
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- products/jasmine_sprout/aospa_jasmine_sprout.mk
+++ products/jasmine_sprout/aospa_jasmine_sprout.mk
@@ -10,6 +10,7 @@ ifeq (aospa_jasmine_sprout,$(TARGET_PRODUCT))
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from the custom device configuration.
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
