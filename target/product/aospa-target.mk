7ed848c1ab3ecbedae9a592e4d1c561d92c57853
Shashank Sivakumar <quic_shassiva@quicinc.com>
2023-06-14 13:50:09 +0530

    aospa: product: Enable Non Flattened APEX
    qssi: Enable Non Flattened APEX
    
    Change-Id: Ic345dd49bc4cc616de273a8414fd8252168dc0e4
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Enable support for APEX updates
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Abstruct
 PRODUCT_PACKAGES += \
     Abstruct
