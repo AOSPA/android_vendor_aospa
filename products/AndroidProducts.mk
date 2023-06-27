38c2929de7ed2fcccb95d1995bfd1b46a454b11d
resist15 <resist15@proton.me>
2023-06-27 09:55:46 +0530

    aospa: products: Introduce realme GT 2 (porsche)
    Change-Id: Ia036ef6603425805accea74601131e1dec20f08b

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -45,6 +45,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/panther/aospa_panther.mk \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
+    $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
@@ -84,6 +85,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_panther-userdebug \
     aospa_phoenix-userdebug \
     aospa_phone1-userdebug \
+    aospa_porsche-userdebug \
     aospa_raven-userdebug \
     aospa_spes-userdebug \
     aospa_sunny-userdebug \
---
+++ products/porsche/aospa_porsche.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_porsche,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/realme/porsche/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := realme
+PRODUCT_DEVICE := porsche
+PRODUCT_MANUFACTURER := realme
+PRODUCT_MODEL := realme GT 2
+PRODUCT_NAME := aospa_porsche
+
+PRODUCT_SYSTEM_NAME := RE58B2L1
+PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Override device name for Play Store.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
