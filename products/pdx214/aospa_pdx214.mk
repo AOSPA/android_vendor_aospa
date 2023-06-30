c454571624480291d155eee38f5c48a3a90fd155
BladeRunner-A2C <john.smith@unused.email>
2023-06-30 15:58:29 +0600

    products: Introduce Sony sagami platform
    - Xperia 1 III (pdx215) & Xperia 5 III (pdx214)
    
    Change-Id: I8e06dd0f9c47055d997fcdb557b65337846583dd
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>

---
+++ products/pdx214/aospa_pdx214.mk
@@ -0,0 +1,29 @@
+#
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq (aospa_pdx214,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/sony/pdx214/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := pdx214
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia 5 III
+PRODUCT_NAME := aospa_pdx214
+
+PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
