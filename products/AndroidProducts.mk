6a853fb86f8ccf6ebaf52d488f8b8fc368b2e86c
CHRISL7 <70589286+CHRISL7@users.noreply.github.com>
2023-08-11 16:29:47 +0000

    products: Add POCO X5 5G and Redmi Note 12 5G
    moonstone: POCO X5 5G (blair = SM6375)
    sunstone: Redmi Note 12 5G (blair_lite = SM4375)
    
    Change-Id: I8ab56f5de80ce4c94a490097581fc39ac62fb56b
    Signed-off-by: CHRISL7 <70589286+CHRISL7@users.noreply.github.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -48,6 +48,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
+    $(LOCAL_DIR)/stone/aospa_stone.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
     $(LOCAL_DIR)/surya/aospa_surya.mk \
     $(LOCAL_DIR)/vayu/aospa_vayu.mk
@@ -88,6 +89,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_porsche-userdebug \
     aospa_raven-userdebug \
     aospa_spes-userdebug \
+    aospa_stone-userdebug \
     aospa_sunny-userdebug \
     aospa_surya-userdebug \
     aospa_vayu-userdebug
