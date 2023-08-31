3f6f9809bc164a0fcf0deb62e3dbcda51e16b8ab
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-31 15:04:15 +0200

    products: Add Xiaomi 13 (fuxi)
    Change-Id: I432f2049eafb529f8cdbe8dd45371bb52e984fbf

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -24,6 +24,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/cheetah/aospa_cheetah.mk \
     $(LOCAL_DIR)/davinci/aospa_davinci.mk \
     $(LOCAL_DIR)/dubai/aospa_dubai.mk \
+    $(LOCAL_DIR)/fuxi/aospa_fuxi.mk \
     $(LOCAL_DIR)/juice/aospa_juice.mk \
     $(LOCAL_DIR)/lisa/aospa_lisa.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
@@ -64,6 +65,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_cheetah-userdebug \
     aospa_davinci-userdebug \
     aospa_dubai-userdebug \
+    aospa_fuxi-userdebug \
     aospa_juice-userdebug \
     aospa_lisa-userdebug \
     aospa_lmi-userdebug \
