bfdaf345aee2943efabbc3eecfeda9e537780f28
electimon <electimon@gmail.com>
2023-10-14 07:47:54 +0800

    aospa: Introduce kernel comparison utility functions
    Change-Id: Ica591b3565fe8db119ea9be867445ead07216865
    Signed-off-by: electimon <electimon@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -263,6 +263,9 @@ PRODUCT_PACKAGES += \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
 
+# Utils
+include vendor/aospa/build/utils.mk
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client
