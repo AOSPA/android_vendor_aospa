435eece09e2639ec4f43773d419e7dc99ed27d37
redducc <sushmit718@gmail.com>
2023-10-29 15:32:29 +0100

    aospa: products: Introduce Redmi 12 5G / POCO M6 Pro (sky)
    Change-Id: I8379effe0606b285b9f12ba01c286d49e5d5a584

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -48,6 +48,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
+    $(LOCAL_DIR)/sky/aospa_sky.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
     $(LOCAL_DIR)/surya/aospa_surya.mk \
     $(LOCAL_DIR)/vayu/aospa_vayu.mk
@@ -88,6 +89,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_porsche-userdebug \
     aospa_raven-userdebug \
     aospa_spes-userdebug \
+    aospa_sky-userdebug \
     aospa_sunny-userdebug \
     aospa_surya-userdebug \
     aospa_vayu-userdebug
