a0cc31df7f8e7d72d34f6fd3c9c4f1bea4dcdf95
TimoDo <timo.capa@gmail.com>
2023-10-30 04:44:00 +0100

    fonts: geist
    values copied from inter
    
    Change-Id: I7f4a41dee6cbce24c9277144a29b658548480598

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -22,6 +22,7 @@ PRODUCT_PACKAGES += \
     AOSPASystemUIOverlay \
     AOSPAWifiOverlay \
     FontGoogleSansLatoOverlay \
+    FontGeistOverlay \
     FontHarmonySansOverlay \
     FontInterOverlay \
     FontManropeOverlay \
