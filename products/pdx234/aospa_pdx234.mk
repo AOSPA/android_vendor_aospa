5edae40b461b43198ff1483d7536d679c5eeb6e9
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-11-20 16:27:10 +0000

    products: Welcome Sony Xperia 1v (pdx234)
    Change-Id: I5f9f21513ed0d0571fd5b5382ab1d8bca4d041f4
    Signed-off-by: SpectreDev1 <Sayf.mohammad01@gmail.com>

---
+++ products/pdx234/aospa_pdx234.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2022-2024 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_pdx234, $(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/sony/pdx234/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := aospa_pdx234
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := pdx234
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := XQ-DQ72
+
+PRODUCT_SYSTEM_NAME := XQ-DQ72
+PRODUCT_SYSTEM_DEVICE := XQ-DQ72
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+endif
