eea717a9de75209924aa6b2886552ebbf6eacd5e
Ahmed Harhash <email@lacentix.me>
2023-12-26 21:32:37 +0200

    build.sh: Don't use --no-clone-bundle on syncing
    * Qualcomm no longer recommends
      this since migrating from CAF to CLO.
    
    Change-Id: Iabd9adab942c6f7a5c3fead8cc73463dd391fc00

--- build.sh
+++ build.sh
@@ -166,7 +166,7 @@ fi
 if [ "$FLAG_SYNC" = 'y' ]; then
         echo -e "${CLR_BLD_BLU}Downloading the latest source files${CLR_RST}"
         echo -e ""
-        repo sync -j"$JOBS" -c --no-clone-bundle --current-branch --no-tags
+        repo sync -j"$JOBS" -c --current-branch --no-tags
 fi
 
 # Check the starting time (of the real build process)
