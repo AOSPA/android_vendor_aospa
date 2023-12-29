1a5428901bbfd402de86272fc394aff920b7a5b9
Jake Weinstein <jake@aospa.co>
2023-12-29 16:54:32 +0900

    aospa: Revert back to 64+32 on all devices
    Some devices use OMX, which does not work under
    64-bit only.
    
    Change-Id: I05489dcbd58c71662f0dab5e66bd5b2510d14ac1

--- products/oneplus9r/aospa_oneplus9r.mk
+++ products/oneplus9r/aospa_oneplus9r.mk
@@ -15,7 +15,7 @@
 ifeq (aospa_oneplus9r,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from the custom device configuration.
