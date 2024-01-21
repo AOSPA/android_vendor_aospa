281a10ad2505666c9e6c67c0f96358717e0ff021
Sugakesshaa <sugakesshaa@pixelos.net>
2024-01-21 19:26:24 +0100

    products: Add Xiaomi 12T Pro (diting)
    Change-Id: I620e9017929eb45c20e0dcb666e877baa4481740

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -22,6 +22,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/bluejay/aospa_bluejay.mk \
     $(LOCAL_DIR)/cheetah/aospa_cheetah.mk \
     $(LOCAL_DIR)/davinci/aospa_davinci.mk \
+    $(LOCAL_DIR)/diting/aospa_diting.mk \
     $(LOCAL_DIR)/dubai/aospa_dubai.mk \
     $(LOCAL_DIR)/juice/aospa_juice.mk \
     $(LOCAL_DIR)/lahaina/aospa_lahaina.mk \
@@ -68,6 +69,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_bluejay-userdebug \
     aospa_cheetah-userdebug \
     aospa_davinci-userdebug \
+    aospa_diting-userdebug \
     aospa_dubai-userdebug \
     aospa_juice-userdebug \
     aospa_lahaina-userdebug \
---
+++ products/diting/aospa_diting.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_diting,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/xiaomi/diting/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := diting
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 22081212UG
+PRODUCT_NAME := aospa_diting
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=diting_global
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+endif
