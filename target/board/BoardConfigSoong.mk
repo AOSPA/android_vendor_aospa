1917a2cfeee21b7788fdd0e7e3e4434fd28c48a6
Mashopy <eliasgheeraert@gmail.com>
2024-01-29 10:59:27 +0100

    aospa: board: Don't set Qualcomm Bluetooth Soong namespaces when QTI BT stack is not used
    Change-Id: I63b580eca977c8d99d1822c89a9b481e849a8994

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -91,6 +91,7 @@ GESTURE_SOONG_VARS := \
 $(foreach v,$(GESTURE_SOONG_VARS),$(eval $(call add-gesturevar-if-exist,$(v))))
 
 # Qualcomm variables
+ifneq ($(TARGET_USE_QTI_BT_STACK),false)
 SOONG_CONFIG_NAMESPACES += aosp_vs_qva
 SOONG_CONFIG_aosp_vs_qva += aosp_or_qva
 SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := qva
@@ -105,4 +106,4 @@ else
     $(warning bt_adv_audio dir is not present)
     SOONG_CONFIG_bredr_vs_btadva_bredr_or_btadva := bredr
 endif #ifneq "$(wildcard vendor/qcom/proprietary/commonsys/bt/bt_adv_audio)" ""
-
+endif
