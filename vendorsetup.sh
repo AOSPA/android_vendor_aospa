c74b1c613ca91980bed1176534d927e6d9932290
Vishalcj17 <vishalcj@aospa.co>
2024-03-22 16:28:10 +0900

    aospa: Build with ThinLTO.
    Change-Id: Ie184b05614747672ea371b818d903d55c890e5a4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,7 @@
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
+
+echo "Building with ThinLTO."
+export GLOBAL_THINLTO=true
+export USE_THINLTO_CACHE=true
