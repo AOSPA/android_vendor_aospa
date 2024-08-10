92359d1e532ba7d759a43820a38eccaf22bc6e50
Omkar Chandorkar <gotenksIN@aospa.co>
2024-08-10 20:50:38 +0900

    Revert "aospa: Disable EAP Proxy."
    - Looks like it's disabled already
    
    wpa_supplicant/android.config
    113:DISABLE_EAP_PROXY=y
    
    This reverts commit 717915c0c2fd4190f24363db7aae6de7b2a8232d.
    
    Change-Id: Ia51e81f04b8f990d1fae7c54f183177d37fb6302

--- target/board/BoardConfigAOSPA.mk
+++ target/board/BoardConfigAOSPA.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Disable EAP Proxy because it depends on proprietary headers
-# and breaks WPA Supplicant compilation.
-DISABLE_EAP_PROXY := true
-
 # Dex2oat
 ifeq ($(TARGET_CPU_VARIANT),cortex-a510)
     DEX2OAT_TARGET_CPU_VARIANT := cortex-a76
