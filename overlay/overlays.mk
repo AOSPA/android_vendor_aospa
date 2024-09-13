393b920d92c315fb7da1d4a46e4f31e7f7f10323
ViShal69x <tk5762996@gmail.com>
2024-09-13 19:42:51 +0000

    aospa: Overlay PermissionController to use headline font from framework resource
    * Fixes headline font being stuck to roboto in Security & Privacy settings.
    
    Change-Id: I5e12555250f543f915e74134745569e3b68d2d29
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/14753
    Reviewed-by: Ralph Chen <ralph950412@gmail.com>
    Tested-by: Ralph Chen <ralph950412@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    ParanoidLauncherOverlay
+    ParanoidLauncherOverlay \
+    PermissionControllerOverlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
