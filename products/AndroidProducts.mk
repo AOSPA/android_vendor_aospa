a6bd7f91bfc979e923e25d3f9ba0f9c332277828
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-09 01:07:27 +0900

    products: Remove un-maintained products
    Change-Id: Idcb0289415a26f264322a9576f94904961cbda2f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,73 +16,52 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/alioth/aospa_alioth.mk \
     $(LOCAL_DIR)/apollo/aospa_apollo.mk \
-    $(LOCAL_DIR)/beryllium/aospa_beryllium.mk \
     $(LOCAL_DIR)/bitra/aospa_bitra.mk \
     $(LOCAL_DIR)/bladerunner/aospa_bladerunner.mk \
-    $(LOCAL_DIR)/bluejay/aospa_bluejay.mk \
-    $(LOCAL_DIR)/cheetah/aospa_cheetah.mk \
     $(LOCAL_DIR)/davinci/aospa_davinci.mk \
     $(LOCAL_DIR)/dubai/aospa_dubai.mk \
     $(LOCAL_DIR)/fuxi/aospa_fuxi.mk \
     $(LOCAL_DIR)/ishtar/aospa_ishtar.mk \
-    $(LOCAL_DIR)/juice/aospa_juice.mk \
     $(LOCAL_DIR)/lahaina/aospa_lahaina.mk \
-    $(LOCAL_DIR)/lisa/aospa_lisa.mk \
     $(LOCAL_DIR)/lito/aospa_lito.mk \
     $(LOCAL_DIR)/lmi/aospa_lmi.mk \
-    $(LOCAL_DIR)/lynx/aospa_lynx.mk \
     $(LOCAL_DIR)/marble/aospa_marble.mk \
     $(LOCAL_DIR)/mi439_419/aospa_mi439_419.mk \
     $(LOCAL_DIR)/miatoll/aospa_miatoll.mk \
-    $(LOCAL_DIR)/mini5g/aospa_mini5g.mk \
     $(LOCAL_DIR)/munch/aospa_munch.mk \
     $(LOCAL_DIR)/nuwa/aospa_nuwa.mk \
-    $(LOCAL_DIR)/oneplus5/aospa_oneplus5.mk \
-    $(LOCAL_DIR)/oneplus5t/aospa_oneplus5t.mk \
     $(LOCAL_DIR)/oneplus7/aospa_oneplus7.mk \
     $(LOCAL_DIR)/oneplus7pro/aospa_oneplus7pro.mk \
     $(LOCAL_DIR)/oneplus7t/aospa_oneplus7t.mk \
     $(LOCAL_DIR)/oneplus7tpro/aospa_oneplus7tpro.mk \
     $(LOCAL_DIR)/oneplus9/aospa_oneplus9.mk \
-    $(LOCAL_DIR)/oneplus9r/aospa_oneplus9r.mk \
-    $(LOCAL_DIR)/oriole/aospa_oriole.mk \
-    $(LOCAL_DIR)/panther/aospa_panther.mk \
     $(LOCAL_DIR)/pdx234/aospa_pdx234.mk \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
     $(LOCAL_DIR)/phone1/aospa_phone1.mk \
     $(LOCAL_DIR)/phone2/aospa_phone2.mk \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/psyche/aospa_psyche.mk \
-    $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/sky/aospa_sky.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
     $(LOCAL_DIR)/sunny/aospa_sunny.mk \
-    $(LOCAL_DIR)/surya/aospa_surya.mk \
-    $(LOCAL_DIR)/vayu/aospa_vayu.mk
+    $(LOCAL_DIR)/surya/aospa_surya.mk
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
     aospa_apollo-userdebug \
-    aospa_beryllium-userdebug \
     aospa_bitra-userdebug \
     aospa_bladerunner-userdebug \
-    aospa_bluejay-userdebug \
-    aospa_cheetah-userdebug \
     aospa_davinci-userdebug \
     aospa_dubai-userdebug \
     aospa_fuxi-userdebug \
     aospa_ishtar-userdebug \
-    aospa_juice-userdebug \
     aospa_lahaina-userdebug \
-    aospa_lisa-userdebug \
     aospa_lito-userdebug \
     aospa_lmi-userdebug \
-    aospa_lynx-userdebug \
     aospa_marble-userdebug \
     aospa_mi439_419-userdebug \
     aospa_miatoll-userdebug \
-    aospa_mini5g-userdebug \
     aospa_munch-userdebug \
     aospa_nuwa-userdebug \
     aospa_oneplus5-userdebug \
@@ -92,18 +71,13 @@ COMMON_LUNCH_CHOICES += \
     aospa_oneplus7t-userdebug \
     aospa_oneplus7tpro-userdebug \
     aospa_oneplus9-userdebug \
-    aospa_oneplus9r-userdebug \
-    aospa_oriole-userdebug \
-    aospa_panther-userdebug \
     aospa_pdx234-userdebug \
     aospa_phoenix-userdebug \
     aospa_phone1-userdebug \
     aospa_phone2-userdebug \
     aospa_porsche-userdebug \
     aospa_psyche-userdebug \
-    aospa_raven-userdebug \
     aospa_sky-userdebug \
     aospa_spes-userdebug \
     aospa_sunny-userdebug \
-    aospa_surya-userdebug \
-    aospa_vayu-userdebug
+    aospa_surya-userdebug
