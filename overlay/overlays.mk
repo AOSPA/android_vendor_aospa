9a9a6612cb5f1deb1709d6cb4f7397600267c0a1
Marios Roussos <36817283+DLordStarkiller@users.noreply.github.com>
2024-10-22 19:14:52 +0300

    aospa: fonts: Add Ubuntu
    Change-Id: I60ad47e3caf7acf71b29b7d1f052c387ddd52786
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>
    
    Fixup Ubuntu font weights (#10)
    
    Added aliases for ubuntu font to fix header fonts etc
    
    Change-Id: Id97ca637fafebc51b0914e52ce610879d763556e
    
    Fonts: Added ubuntu-bold in "android.theme.customization.REQUIRED_SYSTEM_FONTS"
    
    Fonts: Added Ubuntu Font License
    
    Change-Id: I054cda6d203c9f89ccd5e1610c38d53ca89634ae
    
    Fonts: Remove unused Ubuntu-Bold.ttf in fonts_customization.xml
    
    Change-Id: I986d78050f1802f194870a61237d04d3a13b547e

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    ParanoidLauncherOverlay
+    ParanoidLauncherOverlay \
+    FontUbuntuOverlay
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
