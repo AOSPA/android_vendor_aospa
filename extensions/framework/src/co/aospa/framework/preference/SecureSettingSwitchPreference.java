e90a6aae8c7be0e96b200e63b8a14f04af3e3185
Fazil Sheik <fazil.sheik96@gmail.com>
2024-12-03 13:18:26 +0530

    aospa: Extensions: Convert to SwitchPreferenceCompat
    Change-Id: I90eca4ce802c5769f55488b80518396c93dcea00
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- extensions/framework/src/co/aospa/framework/preference/SecureSettingSwitchPreference.java
+++ extensions/framework/src/co/aospa/framework/preference/SecureSettingSwitchPreference.java
@@ -20,9 +20,9 @@ package co.aospa.framework.preference;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
-public class SecureSettingSwitchPreference extends SwitchPreference {
+public class SecureSettingSwitchPreference extends SwitchPreferenceCompat {
 
     public SecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
--- extensions/framework/src/co/aospa/framework/preference/SystemSettingSwitchPreference.java
+++ extensions/framework/src/co/aospa/framework/preference/SystemSettingSwitchPreference.java
@@ -18,10 +18,10 @@
 package co.aospa.framework.preference;
 
 import android.content.Context;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 import android.util.AttributeSet;
 
-public class SystemSettingSwitchPreference extends SwitchPreference {
+public class SystemSettingSwitchPreference extends SwitchPreferenceCompat {
 
     public SystemSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
