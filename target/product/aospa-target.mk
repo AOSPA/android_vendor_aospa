f391fc72dfe1479a93b263d55fbc92417bfa7213
idoybh <idoybh2@gmail.com>
2024-12-24 17:53:47 +0530

    aospa: overlay: Enable circle to search
    Change-Id: I76996a75d9af2a9daf79f27c8585323188a4802b
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -149,7 +149,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/aospa/target/config/permissions/default_permissions_com.google.android.deskclock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default_permissions_com.google.android.deskclock.xml \
     vendor/aospa/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
-    vendor/aospa/target/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+    vendor/aospa/target/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+    vendor/aospa/target/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
 
 # Preinstalled Packages
 PRODUCT_COPY_FILES += \
