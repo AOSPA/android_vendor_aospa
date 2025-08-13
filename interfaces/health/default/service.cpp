77ae1b540e453888900bc7f0b595fe52ce5f831f
LuK1337 <priv.luk@gmail.com>
2025-08-13 21:34:36 +0200

    health: Implement IFastCharge interface
    Change-Id: Ib121a036d98fe995a3a9f09b47382de6a1104548

--- interfaces/health/default/service.cpp
+++ interfaces/health/default/service.cpp
@@ -1,23 +1,30 @@
 /*
- * Copyright (C) 2022-2023 The LineageOS Project
+ * Copyright (C) 2022-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "ChargingControl.h"
+#include "FastCharge.h"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 
 using ::aidl::vendor::lineage::health::ChargingControl;
+using ::aidl::vendor::lineage::health::FastCharge;
 
 int main() {
     ABinderProcess_setThreadPoolMaxThreadCount(0);
     std::shared_ptr<ChargingControl> lh = ndk::SharedRefBase::make<ChargingControl>();
+    std::shared_ptr<FastCharge> fc = ndk::SharedRefBase::make<FastCharge>();
 
-    const std::string instance = std::string() + ChargingControl::descriptor + "/default";
+    std::string instance = std::string() + ChargingControl::descriptor + "/default";
     binder_status_t status = AServiceManager_addService(lh->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    instance = std::string() + FastCharge::descriptor + "/default";
+    status = AServiceManager_addService(fc->asBinder().get(), instance.c_str());
     CHECK_EQ(status, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
